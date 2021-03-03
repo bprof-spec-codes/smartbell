3e9d0dcab7278d8875e1e646f943fd94f8970665
Ács Péter <parasztragu@gmail.com>
2021-03-03 14:52:38 +0100

    BellRing repository added

---
+++ SmartBell/Repository/IBellRingRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IBellRingRepository : IRepository<BellRing>
+    {
+        void SetIntervalByAudio(string Id);
+    }
+    public class BellRingRepository : Repository<BellRing>, IBellRingRepository
+    {
+        public BellRingRepository(DbContext context) : base(context)
+        {
+        }
+
+        public override BellRing GetOne(string id)
+        {
+            return GetAll().SingleOrDefault(x => x.Id == id);
+        }
+
+        public void SetIntervalByAudio(string Id)
+        {
+            var Bellring = GetOne(Id);
+            if (Bellring == null)
+            {
+                return;
+            }
+
+            // TODO: If possible Interval should be able to be set by the mp3 file's length 
+            /*
+            Bellring.Interval = ???;
+             */
+        }
+    }
+}
---
+++ SmartBell/Repository/IRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        void Insert(T entity);
+
+        T GetOne(string id);
+
+        IQueryable<T> GetAll();
+
+        void Delete(T entity);
+
+        void SaveChanges();
+
+
+    }
+    public abstract class Repository<T> : IRepository<T> where T:class
+    {
+        protected DbContext context;
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+
+        public abstract T GetOne(string id);
+        
+
+        public void Insert(T entity)
+        {
+            context.Set<T>().Add(entity);
+            SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+    }
+}
