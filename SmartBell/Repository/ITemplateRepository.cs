6b5da2a2a372f86a968fed843ff291f1e5c0cf61
Bogya Roland <roy.bogya@gmail.com>
2021-03-04 14:33:47 +0100

    Template repository setup for initial state

---
+++ SmartBell/Repository/ITemplateRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    interface ITemplateRepository : IRepository<Template>
+    {
+    }
+
+    public class TemplateRepository : Repository<Template>, ITemplateRepository
+    {
+        public TemplateRepository(DbContext context) : base(context)
+        {
+        }
+
+        public override Template GetOne(string id)
+        {
+            return GetAll().SingleOrDefault(x => x.Id == id);
+        }
+    }
+}
