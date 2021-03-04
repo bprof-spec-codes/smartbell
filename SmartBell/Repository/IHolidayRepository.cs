9e9b2556639b2340ef5e412b8f635943e9e4a98f
Bogya Roland <roy.bogya@gmail.com>
2021-03-04 14:30:32 +0100

    Holiday repository setup for initial state, azure server dependency added

---
+++ SmartBell/Repository/IHolidayRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    interface IHolidayRepository : IRepository<Holiday>
+    {
+
+    }
+    public class HolidayRepository : Repository<Holiday>, IHolidayRepository
+    {
+        public HolidayRepository(DbContext context) : base(context)
+        {
+        }
+
+        public override Holiday GetOne(string id)
+        {
+            return GetAll().SingleOrDefault(x => x.Id == id);
+        }
+    }
+}
