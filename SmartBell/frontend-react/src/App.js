999861aaf92169c646ea76d2a55b8c47126d6f95
Tóth Áron <conoren@gmail.com>
2021-03-16 11:15:01 +0100

    changes to components

--- SmartBell/frontend-react/src/App.js
+++ SmartBell/frontend-react/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+import TimeTable from './containers/TimeTable/TimeTable';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Smartbell React frontend.</h1>
-        <p>Working.</p>
+        <Layout>
+          <h1>Smartbell React frontend.</h1>
+          <TimeTable/>
+        </Layout>
+        
       </div>
     );
   }
