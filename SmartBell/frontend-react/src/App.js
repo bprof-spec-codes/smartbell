84b8cb1961c3580fe4a0f6f8bf32aa778bf5d67c
Tóth Áron <conoren@gmail.com>
2021-03-16 09:31:30 +0100

    added Ubuntu fonts, new title

--- SmartBell/frontend-react/src/App.js
+++ SmartBell/frontend-react/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Smaertbell React frontend.</h1>
+      <div>
+        <h1>Smartbell React frontend.</h1>
         <p>Working.</p>
       </div>
     );
