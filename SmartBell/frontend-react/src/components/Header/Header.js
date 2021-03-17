a132b474480a7be6679984bad7cdda67fe095e07
Tóth Áron <conoren@gmail.com>
2021-03-18 00:06:48 +0100

    added header for calendar

---
+++ SmartBell/frontend-react/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//rafce -->tabtab
+const Header = () => {
+    return (
+        <header>
+            <h1>Naptár</h1>
+        </header>
+    )
+}
+
+export default Header
