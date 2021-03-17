bd8efd00e37e26c6d32e61093a3a89f27a7e464f
Tóth Áron <conoren@gmail.com>
2021-03-17 19:46:46 +0100

    removed wrong calendar component

--- SmartBell/frontend-react/src/index.js
+++ SmartBell/frontend-react/src/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 //calendar
 import Calendar from './components/Calendar';
 
 //calendar miatt off
-//ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
-
-//calendar
-function App() {
-    return (
-      <div className="App">
-        <Calendar />
-      </div>
-    );
-  }
-  
-  const rootElement = document.getElementById("root");
-  ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
