732f329d71bbd2d50c8f68f19601a014fa570182
Tóth Áron <conoren@gmail.com>
2021-03-18 00:51:20 +0100

    changed the theme from blue to orange

--- SmartBell/frontend-react/src/index.js
+++ SmartBell/frontend-react/src/index.js
@@ -4,9 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//calendar
-import Calendar from './components/Calendar';
-
-//calendar miatt off
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
