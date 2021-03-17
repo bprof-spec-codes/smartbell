ef43f14e698e6b663ee28fd4f1025ee177b92e03
Tóth Áron <conoren@gmail.com>
2021-03-17 19:22:05 +0100

    added calendar component

--- SmartBell/frontend-react/src/index.js
+++ SmartBell/frontend-react/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//calendar
+import Calendar from './components/Calendar';
+
+//calendar miatt off
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
+
+//calendar
+function App() {
+    return (
+      <div className="App">
+        <Calendar />
+      </div>
+    );
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
