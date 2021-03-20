f3e1d1115870d8f051af68cd91e819f8382a602d
Ács Péter <parasztragu@gmail.com>
2021-03-20 18:32:43 +0100

    Template model documentation added

--- SmartBell/Models/Template.cs
+++ SmartBell/Models/Template.cs
@@ -7,14 +7,18 @@ using System.Text.Json.Serialization;
 
 namespace Models
 {
+    /// <summary>Template has a pattern of bellrings for a day.</summary>
     public class Template
     {
+        /// <summary>Unique Id used for database storage.</summary>
         [Key]
         public string Id { get; set; }
 
+        /// <summary>Specifies the name of a bellringing pattern.</summary>
         [StringLength(50)]
         public string Name { get; set; }
 
+        /// <summary>Database storage reference for pattern elements.</summary>
         [NotMapped]
         [JsonIgnore]
         public virtual ICollection<TemplateElement> TemplateElements { get; set; }
