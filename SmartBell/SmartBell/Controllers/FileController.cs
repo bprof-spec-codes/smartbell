5619b13321ea60572f0d05f0a12c9a8ab9e02cc9
Ács Péter <parasztragu@gmail.com>
2021-03-20 21:41:37 +0100

    file controller added

---
+++ SmartBell/SmartBell/Controllers/FileController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace SmartBell.Controllers
+{
+    [ApiController]
+    [Route("File")]
+    public class FileController : ControllerBase
+    {
+        [HttpPost,DisableRequestSizeLimit]
+        public IActionResult Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = "Output";
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length>0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullpath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+
+                    using (var stream = new FileStream(fullpath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok(new { dbPath });
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500,$"Internal server error : {ex}")
+            }
+        }
+    }
+}
