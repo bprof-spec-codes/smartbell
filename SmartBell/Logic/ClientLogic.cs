67ebfa0b632d1b34f3fdbcdef89d761909718156
Ács Péter <parasztragu@gmail.com>
2021-03-20 20:18:00 +0100

    Client logic added

---
+++ SmartBell/Logic/ClientLogic.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class ClientLogic
+    {
+        ModificationLogic modLogic;
+
+        public ClientLogic(ModificationLogic modLogic)
+        {
+            this.modLogic = modLogic;
+        }
+
+        public Template GetTemplateByName(string name)
+        {
+            return modLogic.GetAllTemplate().Where(x => x.Name == name).FirstOrDefault();  
+        }
+        public IQueryable<TemplateElement> GetElementsForTemplate(string id)
+        {
+            return modLogic.GetElementsForTemplate(id);
+        }
+        public IQueryable<BellRing> GetBellRingsForDay(DateTime dayDate)
+        {
+            return modLogic.GetBellRingsForDay(dayDate);
+        }
+    }
+}
