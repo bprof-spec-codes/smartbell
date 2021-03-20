0b17f41fffc8a8d41aad1a703a1e9c399db9847b
Ács Péter <parasztragu@gmail.com>
2021-03-20 21:18:34 +0100

    holiday controller added

---
+++ SmartBell/SmartBell/Controllers/HolidayController.cs
@@ -0,0 +1,45 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartBell.Controllers
+{
+    [ApiController]
+    [Route("Holiday")]
+    public class HolidayController : ControllerBase
+    {
+        ModificationLogic logic;
+
+        public HolidayController(ModificationLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteHoliday(string id)
+        {
+            Holiday holiday = logic.GetOneHoliday(id);
+            logic.DeleteHoliday(holiday);
+        }
+        [HttpGet("{id}")]
+        public Holiday GetHoliday(string id)
+        {
+            return logic.GetOneHoliday(id);
+        }
+        [HttpGet]
+        public IEnumerable<Holiday> GetAllHolidays()
+        {
+            return logic.GetAllHoliday();
+        }
+
+        [HttpPost]
+        public void AddHoliday([FromBody] BellRing item)
+        {
+            logic.InsertBellRing(item);
+        }
+    }
+}
