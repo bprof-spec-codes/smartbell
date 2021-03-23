5f69d6aa650020f4974632109f8ba7916b5c7d7d
Ács Péter <parasztragu@gmail.com>
2021-03-24 00:41:09 +0100

    Migration implemented for database update

--- SmartBell/SmartBell/Migrations/20210323232706_Outputpathmodeladd.Designer.cs
+++ SmartBell/SmartBell/Migrations/20210323232706_Outputpathmodeladd.Designer.cs
@@ -4,14 +4,16 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace SmartBell.Migrations
 {
     [DbContext(typeof(SbDbContext))]
-    partial class SbDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210323232706_Outputpathmodeladd")]
+    partial class Outputpathmodeladd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
