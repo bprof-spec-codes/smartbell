f622c29b64557634232e9d01ed3954409e41d2fd
Bogya Roland <roy.bogya@gmail.com>
2021-03-26 00:17:02 +0100

    PostTTS method added

--- SmartBell/SmartBell/Controllers/BellRingController.cs
+++ SmartBell/SmartBell/Controllers/BellRingController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -41,6 +42,30 @@ namespace SmartBell.Controllers
         public void AddBellRing([FromBody] BellRing item)
         {
             logic.InsertBellRing(item);
+        }
+
+        [HttpPost("PostTTSString/{content} & {filename}")]
+        public IActionResult PostTTSFileFromString(string content,string filename)
+        {
+            try
+            {
+                var folderName = "Output";
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+
+                var fullpath = Path.Combine(pathToSave, filename);
+                if (!System.IO.File.Exists(fullpath))
+                {
+                    System.IO.File.WriteAllText(fullpath, content);
+                    return Ok();
+                }
+                return BadRequest("File already exists.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+            
         }
     }
 }
