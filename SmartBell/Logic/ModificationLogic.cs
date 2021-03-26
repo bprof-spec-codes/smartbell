217f2804d74578b7aa810c0fb176d451ff0322e0
Ács Péter <parasztragu@gmail.com>
2021-03-26 16:29:13 +0100

    This method is incorrect causes problems with updating...

--- SmartBell/Logic/ModificationLogic.cs
+++ SmartBell/Logic/ModificationLogic.cs
@@ -32,12 +32,6 @@ namespace Logic
             }
             bellRingRepo.Insert(bellRing);
         }
-        // For bellrings with multiple seuqenced outputs, with this you can generate a readable method.
-        public void InsertSequencedBellRing(BellRing bellRing,string name)
-        {
-            bellRing.Id = name;
-            bellRingRepo.Insert(bellRing);
-        }
 
         public void InsertHoliday(Holiday holiday)
         {
