2bb5b55aab00b16f574cc1ef2c06d053272d89d5
Tóth Áron <conoren@gmail.com>
2021-03-29 18:15:03 +0200

    added interface where you can add/modify radios

---
+++ SmartBell/frontend-react/src/components/Radio/RadioMaker.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import DDMenu from '../Button/DDMenu'
+
+const RadioMaker = () => {
+    return (
+        <div className='container'>
+            <h1>Rádióműsor-összeállító</h1><br/>
+            <h3>Eddigi rádióműsorjaid: </h3><br/>
+            <DDMenu/>
+            <h3>Kiválaszott rádióműsor:</h3>
+            <p>Zeneszámok: - to be implemented</p>
+            <p>Zeneszámok hozzáadása, eltávolítása: - to be implemented</p><br/>
+            <h3>Új műsor:</h3>
+            <form className='form-control'>
+                <label>Rádióműsor neve</label>
+                    <input  
+                        placeholder='Adj nevet a rádióműsorodnak' 
+                    />
+            <input type='submit' className='btn btn-block' value='Új rádióműsor hozzáadása'/>
+            </form>
+            
+        </div>
+    )
+}
+
+export default RadioMaker
