ffd5e47b2d826c19b9e74927dd6c6847243c17d2
Tóth Áron <conoren@gmail.com>
2021-03-29 17:10:31 +0200

    added option for setting rings for multiple days at once

--- SmartBell/frontend-react/src/components/Calendar/DateChooser.js
+++ SmartBell/frontend-react/src/components/Calendar/DateChooser.js
@@ -7,14 +7,19 @@ import {
     SingleDatePicker
 } from "react-google-flight-datepicker";
 import "react-google-flight-datepicker/dist/main.css";
+import DDMenu from '../Button/DDMenu';
 
 const DateChooser = () => {
     return (
         <div className='dpcontainer'>
+            <h1>Csengetési rend gyorsbeállító</h1><br/>
+            <p>Mettől meddig tartson a csengetési rend?</p><br/>
             <RangeDatePicker
                 startDate={new Date()}
                 endDate={new Date()}
-            />
+            /><br/>
+            <p>Válassz csengetési rendet:</p><br/>
+            <DDMenu/>
         </div>
     )
 }
