0c8d00d61decf464765482fe1c2f53ee860dfb48
Tóth Áron <conoren@gmail.com>
2021-03-29 12:26:20 +0200

    added the calendarConfig js file

---
+++ SmartBell/frontend-react/src/components/Settings/CalendarConfig.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CalendarConfig = () => {
+    return (
+        <footer>
+            <a href='/calendarConfig'>Naptárkezelő</a>
+        </footer>
+    )
+}
+
+export default CalendarConfig
+ 
