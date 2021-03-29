213f0b898041821c4afd5d62468c5292942a8b8f
Tóth Áron <conoren@gmail.com>
2021-03-29 18:15:27 +0200

    added interface where you can upload music

---
+++ SmartBell/frontend-react/src/components/Radio/SoundUploader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SoundUploader = () => {
+    return (
+        <div className='container'>
+            <h1>Zenék kezelése</h1>
+            <p>To be implemented</p>
+        </div>
+    )
+}
+
+export default SoundUploader
