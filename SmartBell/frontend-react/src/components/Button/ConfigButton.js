f0aeaf290d8d5fc8838d2a35968637d9c509f852
Tóth Áron <conoren@gmail.com>
2021-03-29 12:42:02 +0200

    separate component for the config  button

---
+++ SmartBell/frontend-react/src/components/Button/ConfigButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ConfigButton = () => {
+    return (
+        <header>
+            <a href='/calendarConfig'>Naptárkezelő</a>
+        </header>
+    )
+}
+
+export default ConfigButton
+ 
