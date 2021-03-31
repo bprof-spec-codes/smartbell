5dadd9aaa8163b07fa8068050d637fa5fcb82453
Ács Péter <parasztragu@gmail.com>
2021-03-31 15:03:43 +0200

    Client skeleton creatod

---
+++ SmartBellClient/Logic/ReadLogic.cs
@@ -0,0 +1,40 @@
+﻿using Data;
+using Newtonsoft.Json;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Logic
+{
+    public class ReadLogic : IReadLogic
+    {
+        public void GetAllFiles(IList<OutputPath> outputs)
+        {
+            foreach (var item in outputs)
+            {
+                string url = ConstConfig.DomainAddress + @$"\File\filename={item.FilePath}";
+                WebClient wc = new WebClient();
+                //Check if file exists if so check for modification date
+                wc.DownloadFile(url, item.FilePath); //path can be specified here perfectly like : @"c:\myfile.txt"
+            }   
+        }
+
+        public IList<BellRing> GetBellRingsForDay(DateTime dayDate)
+        {
+            string url = ConstConfig.DomainAddress + @$"\Client\GetBellRingsForDay\id={dayDate}";
+            WebClient wc = new WebClient();
+            string jsonContent = wc.DownloadString(url);
+            return JsonConvert.DeserializeObject<List<BellRing>>(jsonContent);
+
+        }
+
+        public IList<OutputPath> GetOutputForBellring(string bellringId)
+        {
+            string url = ConstConfig.DomainAddress + @$"\Client\GetOutputsForBellRing\id={bellringId}";
+            WebClient wc = new WebClient();
+            string jsonContent = wc.DownloadString(url);
+            return JsonConvert.DeserializeObject<List<OutputPath>>(jsonContent);
+        }
+    }
+}
---
+++ SmartBellClient/Repository/ConstConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public static class ConstConfig
+    {
+        public static string DomainAddress= "http://localhost:5000";
+        public static string NtpAddress= "hu.pool.ntp.org";
+
+        public static double Width = 500;
+        public static double Height = 500;
+
+    }
+}
