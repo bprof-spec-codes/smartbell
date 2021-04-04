5ad8b8451ba1463b228d2b945d87681833726364
Áron <conoren@gmail.com>
2021-04-04 16:45:58 +0200

    added sample song array

--- SmartBell/frontend-react/src/components/Radio/RadioMaker.js
+++ SmartBell/frontend-react/src/components/Radio/RadioMaker.js
@@ -1,24 +1,56 @@
 import React from 'react'
 import DDMenu from '../Button/DDMenu'
+import {useState} from 'react'
 
 //rádióműsorokat egytől indexelni
+const radioOptions = ['Alap rádióműsor', 'műsor2', 'műsor3'];
+const radioOption = radioOptions[0];
+
 
 const RadioMaker = () => {
+    const [songs, setSongs] = useState([
+        {
+            id:1,
+            text: 'BasicSong.mp3',
+            length: '3:25',
+            used: false
+        },
+        {
+            id:2,
+            text: 'RockSong.mp3',
+            length: '4:20',
+            used: true
+        },
+        {
+            id:3,
+            text: 'HouseMusic.mp3',
+            length: '10:02',
+            used: false
+        },
+        {
+            id:4,
+            text: 'FunnyFrogSong.mp3',
+            length: '2:44',
+            used: true
+        },
+    ])
+    
     return (
         <div className='container'>
             <h1>Rádióműsor-összeállító</h1><br/>
             <h3>Eddigi rádióműsorjaid: </h3><br/>
-            <DDMenu/>
-            <h3>Kiválaszott rádióműsor:</h3>
+            <DDMenu props={radioOptions} first={radioOption}/>
+            <h3>Kiválaszott rádióműsor zenéi:</h3>
             <p>Zeneszámok: - to be implemented</p>
             <p>Zeneszámok hozzáadása, eltávolítása: - to be implemented</p><br/>
-            <h3>Új műsor:</h3>
             <form className='form-control'>
+                <h3>Új műsor:</h3>
                 <label>Rádióműsor neve</label>
                     <input  
                         placeholder='Adj nevet a rádióműsorodnak' 
                     />
-            <input type='submit' className='btn btn-block' value='Új rádióműsor hozzáadása'/>
+                <input type='submit' className='btn btn-block' value='Új rádióműsor hozzáadása'/>
+            <br/><br/>
             <input type='submit' className='btn btn-block' value='Rádióműsorok mentése'/>
             </form>
             
