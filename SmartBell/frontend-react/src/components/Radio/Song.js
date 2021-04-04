f86d6959defd985f614f8b964ef011a322207618
Áron <conoren@gmail.com>
2021-04-04 17:42:41 +0200

    implemented the song component

--- SmartBell/frontend-react/src/components/Radio/Song.js
+++ SmartBell/frontend-react/src/components/Radio/Song.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa'
 
-const Song = () => {
+
+const Song = ({song,onDelete}) => {
     return (
-        <div>
-            
+        <div className={`task ${song.used? 'reminder':''}`}>
+            <h3>
+                {song.text} {' '}
+                <FaTimes 
+                    style={{color: 'red', cursor: 'pointer'}}
+                    onClick={() => onDelete(song.id)}
+                />
+            </h3>
+            <p>{song.length}</p>
         </div>
     )
 }
